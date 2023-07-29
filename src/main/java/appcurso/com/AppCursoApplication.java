19f42d33eafe94efa72e3299c94e5b3c1f81aed6
codeblueperu <jhoningaaquino@gmail.com>
2023-07-28 22:40:17 -0500

    ultimo

---
+++ src/main/java/appcurso/com/AppCursoApplication.java
@@ -0,0 +1,13 @@
+package appcurso.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/appcurso/com/AppCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package appcurso.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
